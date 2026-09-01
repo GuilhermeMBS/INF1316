d560709ed12c6bb04f1502257a490a0476673ca1
João Pedro Capechi Telhado <126024484+joaocapechi@users.noreply.github.com>
2026-09-01 15:05:24 +0000

    Lab 03 - q1

--- lab03/q1/ctrl-c.c
+++ lab03/q1/ctrl-c.c
@@ -6,24 +6,54 @@
 // #include <sys/ipc.h>
 // #include <sys/shm.h>
 // #include <sys/stat.h>
-// #include <unistd.h>
+#include <unistd.h>
 // #include <sys/wait.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <signal.h>
 
+#define automatico 1
 
 void funcaoTratadora (int sinal) {
     // tratamento do(s) sinal(is)
+    printf("Sinal: %d\n", sinal);
+    switch (sinal)
+    {
+        // Ctrl-C
+        case SIGINT:
+            puts("Ctrl-C evitado\n");
+            break;
+        // Ctrl-\ ()
+        default:
+            puts("Terminando a execução\n");
+            exit(0);
+    }
 }
 
 int main (void) {
     // captura Ctrl-C (SIGINT)
     if (signal(SIGINT, funcaoTratadora) == SIG_ERR) {
         // erro ao instalar a rotina de atendimento do sinal SIGINT
+        puts("Erro ao alocar o handler do signal\n");
+        exit(1);
     }
-    // captura Ctrl-\ (SIGINT)
-    else if (signal(SIGABRT, funcaoTratadora) == SIG_ERR) {
-        // erro ao instalar a rotina de atendimento do sinal SIGINT
+    // captura Ctrl-\ (SIGABRT; SIGQUIT - codespaces)
+    else if (signal(SIGQUIT, funcaoTratadora) == SIG_ERR) {
+        // erro ao instalar a rotina de atendimento do sinal SIGQUIT
+        puts("Erro ao alocar o handler do signal\n");
+        exit(1);
     }
+    
+    #ifdef automatico
+        // Teste automático
+        sleep(2);
+        raise(SIGINT);
+        sleep(2);
+        raise(SIGQUIT);
+    #else
+        // Teste com o terminal
+        for(;;);
+    #endif
+
+    
 }
