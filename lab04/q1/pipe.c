e4dd22096c2883454eb42aca4cb24937b0ee7eb5
João Pedro Capechi Telhado <126024484+joaocapechi@users.noreply.github.com>
2026-09-15 14:58:10 +0000

    Init lab04 q1

---
+++ lab04/q1/pipe.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define TAM_MSG 16
+
+int main() {
+    int fd[2];
+    if (pipe(fd) < 0)
+    {
+        puts("Erro ao abrir a pipe");
+        exit(1);
+    }
+
+    pid_t pid;
+    if ((pid = fork()) < 0)
+    {
+        puts("Erro ao criar o filho");
+        exit(2);
+    }
+    else if (pid == 0)
+    {
+        // Filho
+        close(fd[0]);
+        const char txt_TX[TAM_MSG] = "Ola mundo!";
+        write(fd[1], txt_TX, sizeof txt_TX);
+        printf("Foram escritos %d dados\n", (int)sizeof txt_TX);
+        close(fd[1]);
+    }
+    else 
+    {
+        // Pai
+        close(fd[1]);
+        char txt_RX[TAM_MSG];
+        int qtd = read(fd[0], txt_RX, sizeof txt_RX);
+        printf("Foram lidos %d dados\n", qtd);
+        printf("%s\n", txt_RX);
+        close(fd[0]);
+    }
+    return 0;
+}
